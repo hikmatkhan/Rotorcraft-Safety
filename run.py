cdf99b3f9bf67675c5d62dbfbf207e1971b8808c
danielenricocahall <danielenricocahall@gmail.com>
2021-05-16 17:33:49 -0400

    update pipenv and run

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import requests
 import structlog
 
 _LOGGER = structlog.get_logger(__file__)
+HEADER_COLUMN = 12
 
 
 def download_file(url: str, local_dir: str = '.', local_filename: str = '') -> str:
@@ -22,12 +23,13 @@ def download_file(url: str, local_dir: str = '.', local_filename: str = '') -> s
     if os.path.exists(f'{local_dir}/{local_filename}'):
         _LOGGER.info(f'{local_dir}/{local_filename} already exists. Skipping download.')
     else:
+        _LOGGER.info(f"Downloading file from {url} to {local_dir}/{local_filename}.")
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
             with open(f'./{local_dir}/{local_filename}', 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192):
+                for chunk in r.iter_content(chunk_size=128):
                     f.write(chunk)
-        _LOGGER.info(f'Successfully saved file to {local_dir}/{local_filename}')
+        _LOGGER.info(f"Finished saving file from {url} to {local_dir}/{local_filename}.")
     return f'{local_dir}/{local_filename}'
 
 
@@ -49,15 +51,40 @@ def load_data(path_to_file: str) -> pd.DataFrame:
     :param path_to_file: path to excel file
     :return: Pandas dataframe containing contents of excel spreadsheet
     """
-    _LOGGER.info(f"Loading the excel data from {path_to_file} into a dataframe - this may take a while.")
-    return pd.read_excel(path_to_file, engine='openpyxl')
+    _LOGGER.info(f"Started loading the excel data from {path_to_file} into a dataframe - this may take a while. "
+                 f"You may want to grab a coffee.")
+    df = pd.read_excel(path_to_file, engine='openpyxl', header=HEADER_COLUMN)
+    _LOGGER.info(f"Finished loading the excel data from {path_to_file} into a dataframe.")
+    df = sanitize_df(df)
+    return df
+
+
+def sanitize_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans up column names and elements in a dataframe by stripping out whitespaces/delimiters, etc
+    :param df: dataframe we're operating on/cleaning
+    :return: dataframe with cleaned up columns and elements
+    """
+    df = df.rename(columns=lambda x: x.strip() if isinstance(x, str) else x)
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    return df
+
+
+def predict(text: str):
+    FAULTY_PATTERN = ['OPS CHECKED GOOD', 'OPS CHECKS GOOD',
+                      'OPS CHECK GOOD', 'OPS CHECKS WERE GOOD', 'OPS CHECK AND LEAK CHECK GOOD',
+                      'OPS CHECK NORMAL', 'OPS CHECKS NORMAL', 'OPS CHECKED NORMAL']
+    if any(pattern in text for pattern in FAULTY_PATTERN):
+        return {'Text reflecting condition of failed part': 'FAULTY'}
+    return {'Text reflecting condition of failed part': 'UNKNOWN'}
 
 
 if __name__ == "__main__":
     local_dir = './data'
     # download the file
-    path_to_downloaded_zip_file = download_file('https://www.fire.tc.faa.gov/zip/MasterModelVersion3DDeliverable.zip', local_dir)
+    path_to_downloaded_zip_file = download_file('https://www.fire.tc.faa.gov/zip/MasterModelVersion3DDeliverable.zip',
+                                                local_dir)
     # unzip the file
     path_to_file = unzip_file(path_to_downloaded_zip_file, local_dir)
-    print(path_to_file)
-    load_data(path_to_file)
+    df = load_data(path_to_file)
+    print("")
